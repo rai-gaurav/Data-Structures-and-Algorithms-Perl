fc60c6cd86b60031fb16bac9fab90efed7ea5884
grai <gauravrai7860@gmail.com>
2020-01-04 22:07:16 +0530

    updated with perltidy

--- Sorting/bubble_sort.pl
+++ Sorting/bubble_sort.pl
@@ -12,20 +12,21 @@ sub bubble_sort {
     # Multiple way to get size of array in Perl
     # 1. scalar( @{$arr} )  => Standard way
     # 2. $#$arr + 1  => $#arr will return the last index of array, hence adding 1 to get the actual size
-    my $size    = scalar( @{$arr} );
+    my $size    = scalar(@{$arr});
     my $swapped = 1;
 
     while ($swapped) {
         $swapped = 0;
-        for my $i ( 1 .. $size - 1 ) {
+        for my $i (1 .. $size - 1) {
+
             # Check whether is pair is out of order
             # If yes, swapped them and remember something changed
-            if ( $arr->[ $i - 1 ] > $arr->[$i] ) {
-                ( $arr->[ $i - 1 ], $arr->[$i] ) =
-                  ( $arr->[$i], $arr->[ $i - 1 ] );
+            if ($arr->[$i - 1] > $arr->[$i]) {
+                ($arr->[$i - 1], $arr->[$i]) = ($arr->[$i], $arr->[$i - 1]);
                 $swapped = 1;
             }
         }
+
         # The bubble sort algorithm can be easily optimized by observing that the n-th pass finds the
         # n-th largest element and puts it into its final place. So, the inner loop can avoid looking
         # at the last n − 1 items when running for the n-th time
@@ -35,18 +36,18 @@ sub bubble_sort {
 
 sub print_array {
     my ($arr) = @_;
-    my $size = scalar( @{$arr} );
-    for my $i ( 0 .. $size - 1 ) {
+    my $size = scalar(@{$arr});
+    for my $i (0 .. $size - 1) {
         print " " . $arr->[$i];
     }
 }
 
-my @array = ( 5, 1, 4, 2, 8, 3, 10 );
+my @array = (5, 1, 4, 2, 8, 3, 10);
 
 print "\nUnsorted array is: ";
-print_array( \@array );
+print_array(\@array);
 
-bubble_sort( \@array );
+bubble_sort(\@array);
 
 print "\nSorted array is: ";
-print_array( \@array );
+print_array(\@array);
