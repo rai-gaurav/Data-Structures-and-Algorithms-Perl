b61b5fe304d2d391903f48f6a91036145b08b77f
Gaurav Rai <gauravrai7860@gmail.com>
2020-01-15 20:55:05 +0530

    Completed stack.pl

--- Data-Structures/stack/stack.pl
+++ Data-Structures/stack/stack.pl
@@ -1,1 +1,27 @@
+#!/usr/bin/env perl
 
+use strict;
+use warnings;
+
+my @stack = (1, 4, 6, 12, 3, 56);
+
+print "\nCurrent Stack: @stack";
+print "\nBottom stack item: $stack[0]";
+print "\nTop stack item: $stack[-1]\n";
+
+# Pushing an element
+my $element_to_push = 100;
+push @stack, $element_to_push;
+print "\nElement inserted: $element_to_push";
+print "\nStack after pushing: @stack \n";
+
+# Popping an element
+my $popped_element = pop @stack;
+print "\nElement removed: $popped_element";
+print "\nStack after popping: @stack \n";
+
+# Pushing an array
+my @array_to_push = ("abc", "xyz");
+push @stack, @array_to_push;
+print "\nInserted array elements: @array_to_push";
+print "\nStack after pushing: @stack \n";
