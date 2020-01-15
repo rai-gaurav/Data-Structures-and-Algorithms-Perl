09fde90db9d45123c49ef1f1b9ee6a0eda0e318f
grai <gauravrai7860@gmail.com>
2020-01-15 11:54:24 +0000

    Update array.pl

--- Data-Structures/arrays/array.pl
+++ Data-Structures/arrays/array.pl
@@ -1,1 +1,37 @@
+#!/usr/bin/env perl
+# More info - https://perlmaven.com/perl-arrays
 
+use strict;
+use warnings;
+
+my @names = ("John", "Jane", "Johnny");
+
+# Another way to initialize array
+#my @names = qw/John Jane/;
+
+print "\nElement at index 0: " . $names[0];
+print "\nElement at index 1: " . $names[1];
+print "\nSize of array: " . scalar(@names);
+print "\nMaximun Index of array: " . $#names;
+
+# Adding an element to the end of array
+push @names, "Doe";
+print "\nCurrent array elements after push operation: @names";
+
+# Adding an element to the start of array
+unshift @names, "Joe";
+print "\nCurrent array elements after unshift operation: @names";
+
+# Removing an element to the end of array
+pop @names;
+print "\nCurrent array elements after pop operation: @names";
+
+# Removing an element to the start of array
+shift @names;
+print "\nCurrent array elements after shift operation: @names";
+
+# Slicing array elements, remember index starts at 0
+print "\nSlicing first 2 elements from array: @names[0, 1]";
+
+# Another way - Using range operator
+print "\nSlicing first 2 elements from array using range operator: @names[0 .. 1]";
