5087409e675495fd9a273db7e14680e26313c23a
grai <gauravrai7860@gmail.com>
2020-01-22 13:30:38 +0000

    Update depth_first_search.pl

--- Graphs/depth_first_search.pl
+++ Graphs/depth_first_search.pl
@@ -1,1 +1,62 @@
+#!/usr/bin/env perl
+# https://en.wikipedia.org/wiki/Depth-first_search
 
+package Graph;
+use strict;
+use warnings;
+
+sub new {
+    my ($class) = @_;
+    my $self = bless {}, $class;
+    $self->{graph} = [];
+    return $self;
+
+}
+
+sub add_edge {
+    my ($self, $u, $v) = @_;
+    push @{$self->{graph}[$u]}, $v;
+}
+
+sub print_edge {
+    my ($self) = @_;
+    my $index = 0;
+    foreach my $childs (@{$self->{graph}}) {
+        if (defined $childs and @{$childs} != 0) {
+            print "\nNode $index has neighbours: @$childs";
+        }
+        $index++;
+    }
+}
+
+sub dfs_recursive {
+    my ($self, $source, $visited) = @_;
+    $visited->[$source] = 1;
+    print "\n $source ";
+    for my $neighbour (@{$self->{graph}[$source]}) {
+        if ($visited->[$neighbour] == 0) {
+            $self->dfs_recursive($neighbour, $visited);
+        }
+    }
+}
+
+sub depth_first_search {
+    my ($self, $source) = @_;
+    my @visited = (0) x scalar(@{$self->{graph}});
+    $self->dfs_recursive($source, \@visited);
+}
+
+package main;
+my $graph = Graph->new();
+$graph->add_edge(0, 1);
+$graph->add_edge(0, 2);
+$graph->add_edge(0, 3);
+$graph->add_edge(1, 4);
+$graph->add_edge(2, 4);
+$graph->add_edge(3, 3);
+$graph->add_edge(4, 4);
+
+$graph->print_edge();
+
+print "\nDFS traversal staring from vertex 2: ";
+$graph->depth_first_search(0);
