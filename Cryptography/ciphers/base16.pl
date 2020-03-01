a93a8ae8e3f774956d276e9d9f16463d8cd3753f
unknown <gauravrai7860@gmail.com>
2020-03-01 20:40:16 +0530

    Added base16 cipher

--- Cryptography/ciphers/base16.pl
+++ Cryptography/ciphers/base16.pl
@@ -1,1 +1,17 @@
+#!/usr/bin/env perl
+# https://en.wikipedia.org/wiki/Hexadecimal
+# Mentioned in - https://github.com/sudo-batman/Data-Structures-and-Algorithms-Perl/blob/master/conversions/decimal_to_hexadecimal.pl
 
+use strict;
+use warnings;
+
+sub main {
+    my ($num) = @_;
+    my $hex_num = sprintf("%X", $num);
+    print "\nBase16 encoded is: " . $hex_num;
+
+    my $dec_num = hex($hex_num);
+    print "\nBase16 decoded is: " . $dec_num;
+}
+
+main(45);
