5e7589527b31adb1669ddc960676022231a1e801
grai <gauravrai7860@gmail.com>
2020-04-05 00:33:52 +0530

    Update permutations.pl

--- Backtracking/permutations.pl
+++ Backtracking/permutations.pl
@@ -2,61 +2,39 @@
 
 # https://en.wikipedia.org/wiki/Permutation
 # https://itnext.io/permutations-combinations-algorithms-cheat-sheet-68c14879aba5
-# Total non repetitive permutations => n!/r!(n-r)!
+# Total non repetitive permutations => n!
 # n - number of options (size of array)
-# r - number of slots (combo length)
 # https://metacpan.org/pod/perlfaq4#How-do-I-permute-N-elements-of-a-list?
 
 use strict;
 use warnings;
 use Data::Dumper;
 
-sub permutation_with_backtracking {
-    my ($arr) = @_;
-    my $size = scalar(@{$arr});
-
-}
+my @permute = ();
 
 sub permutations {
-    my ($arr) = @_;
-    my $size = scalar(@{$arr});
-    if ($size == 0) {
-        return ([]);
+    my ($arr, $start, $end) = @_;
+
+    if ($start == $end) {
+        # Make a deep copy
+        my @smaller_arr = @{$arr};
+        push @permute, \@smaller_arr;
     }
-    elsif ($size == 1) {
-        return ($arr);
+
+    for my $index ($start .. $end) {
+        ($arr->[$start], $arr->[$index]) = ($arr->[$index], $arr->[$start]);
+        permutations($arr, $start + 1, $end);
+        ($arr->[$start], $arr->[$index]) = ($arr->[$index], $arr->[$start]);
     }
-
-    my @permute = ();
-
-    for my $index (0 .. $size - 1) {
-        my $m = $arr->[$index];
-        my @remaining_array;
-        push @remaining_array, (@{$arr}[0 .. $index - 1], @{$arr}[$index + 1 .. $size - 1]);
-        print "Remaining List";
-        print Dumper(\@remaining_array);
-
-        print "Smaller Perm:";
-
-        my ($smaller_perm) = permutations(\@remaining_array);
-
-        #print Dumper($smaller_perm);
-
-        foreach my $small_perm (@{$smaller_perm}) {
-
-            print Dumper($small_perm);
-            push @permute, [$m, $small_perm];
-        }
-    }
-    print "\nPermute:";
-    print Dumper(\@permute);
-    return \@permute;
 }
 
 sub main {
     my @array = (1, 2, 3);
-    my $perm  = permutations(\@array);
-
-    #print Dumper($perm);
+    my $size  = scalar(@array);
+    permutations(\@array, 0, $size - 1);
+    my $total_perm = scalar(@permute);
+    print "\nTotal permutations " . $total_perm . "\n";
+    print Dumper(\@permute);
 }
+
 main;
