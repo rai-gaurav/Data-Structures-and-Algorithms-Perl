071a961abaad3223f4d85e75abffce1e1387fd4e
rai-gaurav <gauravrai7860@gmail.com>
2020-06-23 23:05:06 +0530

    Added client server example

--- Client_Server/client.pl
+++ Client_Server/client.pl
@@ -1,1 +1,31 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use IO::Socket;
 
+local $| = 1;                   # Autoflush
+my $hostname = 'localhost';     # localhost
+my $port     = 7171;
+
+my $socket = IO::Socket::INET->new(
+    PeerAddr => $hostname,
+    PeerPort => $port,
+    Proto => 'tcp',
+    Timeout => 1
+) or die "Could not connect: $!";
+
+while (1) {
+    print "\nEnter the data to send: ";
+    # Take user inout
+    my $send_data = <>;
+    chomp($send_data);
+    if ($send_data) {
+        my $size = $socket->send($send_data);
+        print "\nSent data of length: $size";
+
+        my $buffer;
+        $socket->recv($buffer, 1024);
+        print "\nGot back $buffer\n";
+    }
+}
+$socket->close();
--- Client_Server/server.pl
+++ Client_Server/server.pl
@@ -1,1 +1,47 @@
+#!/usr/bin/env perl
 
+# https://metacpan.org/pod/Socket
+# Socket a low-level module used by the IO::Socket family of modules.
+# A practical program would likely use the higher-level API provided by IO::Socket
+# https://metacpan.org/pod/IO::Socket
+
+use strict;
+use warnings;
+use IO::Socket;
+
+local $| = 1;                # Autoflush
+my $hostname = '0.0.0.0';    # localhost
+my $port     = 7171;
+
+my $socket = IO::Socket::INET->new(
+    LocalHost => $hostname,
+    LocalPort => $port,
+    Proto     => 'tcp',
+    Listen    => 5,
+    Reuse     => 1
+) or die "Cannot create socket: $!";
+
+print "Waiting for tcp connection from client on port $port\n";
+
+while (1) {
+    my $client_socket  = $socket->accept();
+    my $client_address = $client_socket->peerhost;
+    my $client_port    = $client_socket->peerport;
+
+    print "$client_address $client_port has connected";
+
+    while (1) {
+
+        # read up to 1024 characters from the connected client
+        my $data;
+        $client_socket->recv($data, 1024);
+        if ($data) {
+            print "\nReceived data: $data";
+
+            # write response data to the connected client
+            $client_socket->send("OK " . $data);
+        }
+    }
+}
+
+$socket->close;
